d655f8b2e17a18dcb73edbe0e74999f5a5cb3537
proyect75 <ariel@infrasoft.com.ar>
2023-06-21 00:37:18 -0300

    different change in the design and the process

--- media/js/vue.js
+++ media/js/vue.js
@@ -4,3 +4,18 @@
  * Salta Capital
  * Derechos reservados
  **********************************************/
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: "",
+      time_elapsed: new Date().toLocaleString()
+    },
+    created: function () {
+        // `this` hace referencia a la instancia vm
+        console.log('work ')
+      }
+  })
+
+  
+
